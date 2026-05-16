2471576120aed8e000c96af303a34e4811a0a8c2
@ashrocket <ashleyraiteri@gmail.com>
2026-05-16 11:17:23 -0400

    feat(goon): add BandMusicGamesTests target + smoke test

---
+++ ios/BandMusicGamesTests/SmokeTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import BandMusicGames
+
+final class SmokeTests: XCTestCase {
+    func test_smoke_targetIsImportable() {
+        // If this file compiles, the test target can see the app module.
+        XCTAssertTrue(true)
+    }
+}
